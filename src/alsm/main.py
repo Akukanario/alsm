9837c9b53fdda119a856667142a6312857574ecd
Akukanarion <166861017+Akukanario@users.noreply.github.com>
2026-06-07 11:17:30 +0100

    Add files via upload

--- src/alsm/main.py
+++ src/alsm/main.py
@@ -356,3 +356,17 @@ if HAVE_QT:
                 data = dlg.get_data()
             else:
                 return
+
+
+    def gui_main():
+        app = QApplication(sys.argv)
+        w = MainWindow()
+        w.show()
+        sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    if HAVE_QT:
+        gui_main()
+    else:
+        console_main()
